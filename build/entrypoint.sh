834c19267e4444ebcac3c970ca0368cb91f2e7da
David Colburn <xero73@gmail.com>
2022-05-23 12:12:35 -0500

    fix signals

---
+++ build/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Start pulseaudio as system wide daemon; for debugging it helps to start in non-daemon mode
+pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
+
+# Run egress service
+exec ./egress
