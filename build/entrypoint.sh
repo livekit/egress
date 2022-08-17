55dfe618746c5bbba3f8ff7bfce17e7818f514e4
Benjamin Pracht <benjamin@livekit.io>
2022-08-17 11:46:41 -0700

    Set a specific temp directory location and clean it up on startup (#79)

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+export TMPDIR=/tmp/lkegress
+
 # Start pulseaudio as system wide daemon; for debugging it helps to start in non-daemon mode
 pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
+
+# cleanup old temporary files
+if ! [ -z $TMPDIR ]; then
+	rm -rf $TMPDIR
+	mkdir -p $TMPDIR
+fi
 
 # Run egress service
 exec egress
