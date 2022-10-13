8c595b25457365948f016928b54a1feab48d0551
Nikita Konev <nikit.cpp@yandex.ru>
2022-10-14 00:28:36 +0400

    Fix starting stopped container

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,6 +3,9 @@ set -euxo pipefail
 
 export TMPDIR=/tmp/lkegress
 
+# Cleanup to be "stateless" on startup, otherwise pulseaudio daemon can't start again
+rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
+
 # Start pulseaudio as system wide daemon; for debugging it helps to start in non-daemon mode
 pulseaudio -D --verbose --exit-idle-time=-1 --system --disallow-exit
 
