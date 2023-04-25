5f3bcf35dff8849f07edca31bb51b0f8f10c0e4d
paulwe <paulwe@gmail.com>
2023-04-24 21:22:08 -0700

    feedback

--- pkg/config/output.go
+++ pkg/config/output.go
@@ -320,7 +320,7 @@ func (p *PipelineConfig) getSegmentConfig(segments *livekit.SegmentedFileOutput)
 		// The splitMuxSink should request key frames from the encoder at expected frame boundaries.
 		// Set the key frame interval to twice the segment duration as a failsafe
 		p.KeyFrameInterval = 2 * float64(conf.SegmentDuration)
-	} else if p.KeyFrameInterval < float64(conf.SegmentDuration) && p.KeyFrameInterval > 1 {
+	} else if p.KeyFrameInterval < float64(conf.SegmentDuration) && p.KeyFrameInterval >= 1 {
 		conf.SegmentDuration = int(p.KeyFrameInterval)
 	}
 
