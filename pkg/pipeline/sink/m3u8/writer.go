1fd127fe6dccfda487ef422a3d4b0d5c9fa39b21
Benjamin Pracht <benjamin@livekit.io>
2023-08-30 12:25:04 -0700

    WiP

--- pkg/pipeline/sink/m3u8/writer.go
+++ pkg/pipeline/sink/m3u8/writer.go
@@ -15,6 +15,7 @@
 package m3u8
 
 import (
+	"container/list"
 	"fmt"
 	"io/fs"
 	"os"
@@ -23,15 +24,43 @@ import (
 	"time"
 )
 
-type PlaylistWriter struct {
+type PlaylistType string
+
+const (
+	PlaylistTypeLive  PlaylistType = ""
+	PlaylistTypeEvent PlaylistType = "EVENT"
+)
+
+type PlaylistWriter interface {
+	Append(dateTime time.Time, duration float64, filename string) error
+	Close() error
+}
+
+type basePlaylistWriter struct {
 	filename       string
 	targetDuration int
 }
 
-func NewPlaylistWriter(filename string, targetDuration int) (*PlaylistWriter, error) {
-	p := &PlaylistWriter{
-		filename:       filename,
-		targetDuration: targetDuration,
+type eventPlaylistWriter struct {
+	basePlaylistWriter
+}
+
+type livePlaylistWriter struct {
+	basePlaylistWriter
+
+	windowSize int
+	mediaSeq   int
+
+	livePlaylistHeader   string
+	livePlaylistSegments *list.List
+}
+
+func NewEventPlaylistWriter(filename string, targetDuration int) (PlaylistWriter, error) {
+	p := &eventPlaylistWriter{
+		basePlaylistWriter: basePlaylistWriter{
+			filename:       filename,
+			targetDuration: targetDuration,
+		},
 	}
 
 	f, err := os.Create(p.filename)
@@ -40,15 +69,7 @@ func NewPlaylistWriter(filename string, targetDuration int) (*PlaylistWriter, er
 	}
 	defer f.Close()
 
-	var sb strings.Builder
-	sb.WriteString("#EXTM3U\n")
-	sb.WriteString("#EXT-X-VERSION:4\n")
-	sb.WriteString("#EXT-X-PLAYLIST-TYPE:EVENT\n")
-	sb.WriteString("#EXT-X-ALLOW-CACHE:NO\n")
-	sb.WriteString("#EXT-X-MEDIA-SEQUENCE:0\n")
-	sb.WriteString(fmt.Sprintf("#EXT-X-TARGETDURATION:%d\n", p.targetDuration))
-
-	_, err = f.WriteString(sb.String())
+	_, err = f.WriteString(p.createHeader(PlaylistTypeEvent))
 	if err != nil {
 		return nil, err
 	}
@@ -56,14 +77,40 @@ func NewPlaylistWriter(filename string, targetDuration int) (*PlaylistWriter, er
 	return p, nil
 }
 
-func (p *PlaylistWriter) Append(dateTime time.Time, duration float64, filename string) error {
-	f, err := os.OpenFile(p.filename, os.O_WRONLY|os.O_APPEND, fs.ModeAppend)
-	if err != nil {
-		return err
+func NewLivePlaylistWriter(filename string, targetDuration int, windowSize int) (PlaylistWriter, error) {
+	p := &livePlaylistWriter{
+		basePlaylistWriter: basePlaylistWriter{
+			filename:       filename,
+			targetDuration: targetDuration,
+		},
+		windowSize:           windowSize,
+		livePlaylistSegments: list.New(),
 	}
-	defer f.Close()
 
+	p.livePlaylistHeader = p.createHeader(PlaylistTypeLive)
+
+	return p, nil
+}
+
+func (p *basePlaylistWriter) createHeader(plType PlaylistType) string {
 	var sb strings.Builder
+	sb.WriteString("#EXTM3U\n")
+	sb.WriteString("#EXT-X-VERSION:4\n")
+	if plType != PlaylistTypeLive {
+		sb.WriteString(fmt.Sprintf("#EXT-X-PLAYLIST-TYPE:%s\n", plType))
+	}
+	sb.WriteString("#EXT-X-ALLOW-CACHE:NO\n")
+	sb.WriteString(fmt.Sprintf("#EXT-X-TARGETDURATION:%d\n", p.targetDuration))
+	if plType != PlaylistTypeLive {
+		sb.WriteString("#EXT-X-MEDIA-SEQUENCE:0\n")
+	}
+
+	return sb.String()
+}
+
+func (p *basePlaylistWriter) createSegmentEntry(dateTime time.Time, duration float64, filename string) string {
+	var sb strings.Builder
+
 	sb.WriteString("#EXT-X-PROGRAM-DATE-TIME:")
 	sb.WriteString(dateTime.UTC().Format("2006-01-02T15:04:05.999Z07:00"))
 	sb.WriteString("\n#EXTINF:")
@@ -72,12 +119,22 @@ func (p *PlaylistWriter) Append(dateTime time.Time, duration float64, filename s
 	sb.WriteString(filename)
 	sb.WriteString("\n")
 
-	_, err = f.WriteString(sb.String())
+	return sb.String()
+}
+
+func (p *eventPlaylistWriter) Append(dateTime time.Time, duration float64, filename string) error {
+	f, err := os.OpenFile(p.filename, os.O_WRONLY|os.O_APPEND, fs.ModeAppend)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(p.createSegmentEntry(dateTime, duration, filename))
 	return err
 }
 
 // Close sliding playlist and make them fixed.
-func (p *PlaylistWriter) Close() error {
+func (p *eventPlaylistWriter) Close() error {
 	f, err := os.OpenFile(p.filename, os.O_WRONLY|os.O_APPEND, fs.ModeAppend)
 	if err != nil {
 		return err
@@ -86,4 +143,51 @@ func (p *PlaylistWriter) Close() error {
 
 	_, err = f.WriteString("#EXT-X-ENDLIST\n")
 	return err
+}
+
+func (p *livePlaylistWriter) Append(dateTime time.Time, duration float64, filename string) error {
+	f, err := os.OpenFile(p.filename, os.O_WRONLY|os.O_CREATE, 0)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	segmentStr := p.createSegmentEntry(dateTime, duration, filename)
+	p.livePlaylistSegments.PushBack(segmentStr)
+
+	for p.livePlaylistSegments.Len() > p.windowSize {
+		p.livePlaylistSegments.Remove(p.livePlaylistSegments.Front())
+		p.mediaSeq++
+	}
+
+	_, err = f.WriteString(p.generatePlaylist())
+	return err
+}
+
+func (p *livePlaylistWriter) Close() error {
+	f, err := os.OpenFile(p.filename, os.O_WRONLY|os.O_APPEND, fs.ModeAppend)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(p.generatePlaylist())
+	if err != nil {
+		return err
+	}
+
+	_, err = f.WriteString("#EXT-X-ENDLIST\n")
+	return err
+}
+
+func (p *livePlaylistWriter) generatePlaylist() string {
+	var sb strings.Builder
+	sb.WriteString(p.livePlaylistHeader)
+	sb.WriteString(fmt.Sprintf("#EXT-X-MEDIA-SEQUENCE:%d\n", p.mediaSeq))
+	for elem := p.livePlaylistSegments.Front(); elem != nil; elem = elem.Next() {
+		segmentStr := elem.Value.(string)
+		sb.WriteString(segmentStr)
+	}
+
+	return sb.String()
 }
