378c1a8dfe6868ce2527468d1469f9a90c80bda6
Benjamin Pracht <benjamin@livekit.io>
2023-09-25 14:11:45 -0700

    WiP

---
+++ pkg/pipeline/builder/image.go
@@ -0,0 +1,132 @@
+// Copyright 2023 LiveKit, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package builder
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/livekit/egress/pkg/config"
+	"github.com/livekit/egress/pkg/errors"
+	"github.com/livekit/egress/pkg/gstreamer"
+	"github.com/livekit/egress/pkg/types"
+	"github.com/livekit/protocol/utils"
+	"github.com/tinyzimmer/go-gst/gst"
+)
+
+const (
+	imageQueueLatency = uint64(200 * time.Millisecond)
+)
+
+func BuildImageBins(pipeline *gstreamer.Pipeline, p *config.PipelineConfig) ([]*gstreamer.Bin, error) {
+	o := p.GetImageConfigs()
+
+	var bins []*gstreamer.Bin
+	for _, c := range o {
+		b, err := BuildImageBin(c, pipeline, p)
+		if err != nil {
+			return nil, err
+		}
+
+		bins = append(bins, b)
+	}
+
+	return bins, nil
+}
+
+func BuildImageBin(c *config.ImageConfig, pipeline *gstreamer.Pipeline, p *config.PipelineConfig) (*gstreamer.Bin, error) {
+	id := utils.NewGuid("")
+
+	b := pipeline.NewBin(fmt.Sprintf("image_%s", id))
+
+	queue, err := gstreamer.BuildQueue(fmt.Sprintf("image_queue_%s", id), imageQueueLatency, true)
+	if err != nil {
+		return nil, err
+	}
+	if err := b.AddElements(queue); err != nil {
+		return nil, errors.ErrGstPipelineError(err)
+	}
+
+	videoRate, err := gst.NewElement("videorate")
+	if err != nil {
+		return nil, errors.ErrGstPipelineError(err)
+	}
+	if err = videoRate.SetProperty("max-duplication-time", uint64(time.Duration(c.CaptureInterval)*time.Second)); err != nil {
+		return nil, err
+	}
+	if err = videoRate.SetProperty("skip-to-first", true); err != nil {
+		return nil, err
+	}
+	if err := b.AddElements(videoRate); err != nil {
+		return nil, errors.ErrGstPipelineError(err)
+	}
+
+	videoScale, err := gst.NewElement("videoscale")
+	if err != nil {
+		return nil, errors.ErrGstPipelineError(err)
+	}
+	if err := b.AddElements(videoScale); err != nil {
+		return nil, errors.ErrGstPipelineError(err)
+	}
+
+	caps, err := gst.NewElement("capsfilter")
+	if err != nil {
+		return nil, errors.ErrGstPipelineError(err)
+	}
+	err = caps.SetProperty("caps", gst.NewCapsFromString(fmt.Sprintf(
+		"video/x-raw,framerate=1/%d,format=I420,width=%d,height=%d,colorimetry=bt709,chroma-site=mpeg2,pixel-aspect-ratio=1/1",
+		c.CaptureInterval, c.Width, c.Height)))
+	if err != nil {
+		return nil, err
+	}
+	if err := b.AddElements(caps); err != nil {
+		return nil, errors.ErrGstPipelineError(err)
+	}
+
+	switch c.ImageOutCodec {
+	case types.MimeTypeJPEG:
+		enc, err := gst.NewElement("jpegenc")
+		if err != nil {
+			return nil, errors.ErrGstPipelineError(err)
+		}
+		if err := b.AddElements(enc); err != nil {
+			return nil, errors.ErrGstPipelineError(err)
+		}
+	default:
+		return nil, errors.ErrNoCompatibleCodec
+	}
+
+	sink, err := gst.NewElement("multifilesink")
+	if err != nil {
+		return nil, err
+	}
+	err = sink.SetProperty("post-messages", true)
+	if err != nil {
+		return nil, err
+	}
+
+	// File will be renamed if the TS prefix is configured
+	location := fmt.Sprintf("%s_%%05d.ts", c.ImagePrefix)
+
+	err = sink.SetProperty("location", location)
+	if err != nil {
+		return nil, err
+	}
+	if err := b.AddElements(sink); err != nil {
+		return nil, errors.ErrGstPipelineError(err)
+	}
+
+	return b, nil
+}
---
+++ pkg/pipeline/sink/images.go
@@ -0,0 +1,60 @@
+// Copyright 2023 LiveKit, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sink
+
+import (
+	"github.com/livekit/egress/pkg/config"
+	"github.com/livekit/egress/pkg/gstreamer"
+	"github.com/livekit/egress/pkg/pipeline/sink/uploader"
+)
+
+type ImageSink struct {
+	uploader.Uploader
+
+	*config.ImageConfig
+
+	conf      *config.PipelineConfig
+	callbacks *gstreamer.Callbacks
+}
+
+func newImageSink(u uploader.Uploader, p *config.PipelineConfig, o *config.ImageConfig, callbacks *gstreamer.Callbacks) (*ImageSink, error) {
+	return &ImageSink{
+		Uploader:    u,
+		ImageConfig: o,
+		conf:        p,
+		callbacks:   callbacks,
+	}, nil
+}
+
+func (s *ImageSink) Start() error {
+	// TODO setup gst pipeline
+	// TODO filename
+
+	return nil
+}
+
+func (s *ImageSink) NewImage(filepath string, ts uint64) error {
+	// TODO rename file, upload
+
+	return nil
+}
+
+func (s *ImageSink) Close() error {
+
+	return nil
+}
+
+func (*Sink) Cleanup() {
+}
