89c54d0ec63b10fbc88d5019d176d83f723fbb41
David Colburn <xero73@gmail.com>
2024-06-06 16:21:15 -0700

    testing

--- build/chrome/scripts/build.sh
+++ build/chrome/scripts/build.sh
@@ -27,13 +27,17 @@ echo 'solutions = [
   },
 ]' | tee '.gclient' > /dev/null
 cd src || exit
+git fetch --tags
+git checkout -b stable 124.0.6367.201
+gclient sync --with_branch_heads --with_tags
 ./build/install-build-deps.sh
 ./build/linux/sysroot_scripts/install-sysroot.py --arch=arm64
 gclient runhooks
 gn gen out/default --args='target_cpu="arm64" proprietary_codecs=true ffmpeg_branding="Chrome" enable_nacl=false is_debug=false symbol_level=0 v8_symbol_level=0 dcheck_always_on=false is_official_build=true'
 autoninja -C out/default chrome chrome_sandbox
 cd out/default || exit
-zip arm64.zip \
+mkdir "$GITHUB_WORKSPACE"/build/chrome/arm64
+mv "$GITHUB_WORKSPACE"/build/chrome/arm64 \
   chrome \
   chrome-wrapper \
   chrome_sandbox \
