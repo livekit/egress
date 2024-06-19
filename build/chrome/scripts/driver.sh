c4d72967fb49b2666c17f6c7a2e8ed57b561922d
David Colburn <xero73@gmail.com>
2024-06-19 00:12:22 -0700

    use the right chrome driver

--- build/chrome/scripts/driver.sh
+++ build/chrome/scripts/driver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -xeuo pipefail
 
-wget -N https://chromedriver.storage.googleapis.com/2.46/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip -d "$HOME/output"
+wget https://storage.googleapis.com/chrome-for-testing-public/"$1"/linux64/chromedriver-linux64.zip
+unzip chromedriver-linux64.zip -d "$HOME/output/amd64"
+wget https://storage.googleapis.com/chrome-for-testing-public/"$1"/mac-arm64/chromedriver-mac-arm64.zip
+unzip chromedriver-mac-arm64.zip -d "$HOME/output/arm64"
