6b085c41e00ac25061b03c51ffdd2bea4c129b1f
David Colburn <xero73@gmail.com>
2024-06-20 12:56:53 -0700

    remove version from install script

--- build/chrome/scripts/amd64.sh
+++ build/chrome/scripts/amd64.sh
@@ -3,4 +3,4 @@ set -xeuo pipefail
 
 wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_"$1"-1_amd64.deb
 mkdir -p "$HOME/output/amd64"
-mv google-chrome-stable_"$1"-1_amd64.deb "$HOME/output/amd64"
+mv google-chrome-stable_"$1"-1_amd64.deb "$HOME/output/amd64/google-chrome-stable_amd64.deb"
