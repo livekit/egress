cd077eb522c188715e628d13b6186bc467c6f905
David Colburn <xero73@gmail.com>
2024-06-20 12:34:42 -0700

    Full auto chrome builds (#705)
    * automated build
    
    * update to new chrome installer image
    
    * use the right chrome driver
    
    * use the right chrome driver
    
    * sleep after running
    
    * linode-cli create not working
    
    * final fixes

--- build/chrome/scripts/setup.sh
+++ build/chrome/scripts/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -xeuo pipefail
 
 useradd -m -d /home/chrome -s /bin/bash chrome
 mkdir /home/chrome/.ssh
@@ -8,3 +9,6 @@ chmod 700 /home/chrome/.ssh
 chmod 600 /home/chrome/.ssh/authorized_keys
 adduser chrome sudo
 sed -i '54i chrome ALL=(ALL:ALL) NOPASSWD: ALL' /etc/sudoers
+echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
+echo "ClientAliveCountMax 3" >> /etc/ssh/sshd_config
+systemctl restart ssh
