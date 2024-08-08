b24d2d50fcb2f04f4aab777d025fc3483b91e3ea
David Colburn <xero73@gmail.com>
2024-08-08 17:29:27 -0400

    fix SRT test

--- test/stream.go
+++ test/stream.go
@@ -92,6 +92,14 @@ func (r *Runner) runStreamTest(t *testing.T, req *rpc.StartEgressRequest, test *
 
 	// verify
 	r.verifyStreams(t, p, urls[0][2], urls[2][2])
+	if test.outputType != types.OutputTypeRTMP {
+		r.checkStreamUpdate(t, egressID, map[string]livekit.StreamInfo_Status{
+			urls[0][1]: livekit.StreamInfo_ACTIVE,
+			urls[1][1]: livekit.StreamInfo_FAILED,
+			urls[2][1]: livekit.StreamInfo_ACTIVE,
+			urls[3][1]: livekit.StreamInfo_ACTIVE,
+		})
+	}
 	r.checkStreamUpdate(t, egressID, map[string]livekit.StreamInfo_Status{
 		urls[0][1]: livekit.StreamInfo_ACTIVE,
 		urls[1][1]: livekit.StreamInfo_FAILED,
