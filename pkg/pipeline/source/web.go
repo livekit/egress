a6a088f25d00e63eb8bf777fdace097411f2a18d
David Colburn <xero73@gmail.com>
2024-11-12 10:49:30 -0800

    missing import

--- pkg/pipeline/source/web.go
+++ pkg/pipeline/source/web.go
@@ -30,6 +30,7 @@ import (
 
 	"github.com/livekit/egress/pkg/config"
 	"github.com/livekit/egress/pkg/errors"
+	"github.com/livekit/protocol/livekit"
 	"github.com/livekit/protocol/logger"
 	"github.com/livekit/protocol/tracer"
 )
