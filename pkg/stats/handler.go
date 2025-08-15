5c3aab8316c32248d6017ac2f9cc4172851da779
David Colburn <xero73@gmail.com>
2025-08-15 15:54:07 -0400

    remove egressID from all handler prom metrics (#962)

--- pkg/stats/handler.go
+++ pkg/stats/handler.go
@@ -24,10 +24,10 @@ type HandlerMonitor struct {
 	backupCounter       *prometheus.CounterVec
 }
 
-func NewHandlerMonitor(nodeId string, clusterId string, egressId string) *HandlerMonitor {
+func NewHandlerMonitor(nodeId string, clusterId string) *HandlerMonitor {
 	m := &HandlerMonitor{}
 
-	constantLabels := prometheus.Labels{"node_id": nodeId, "cluster_id": clusterId, "egress_id": egressId}
+	constantLabels := prometheus.Labels{"node_id": nodeId, "cluster_id": clusterId}
 
 	m.uploadsCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace:   "livekit",
@@ -75,26 +75,26 @@ func (m *HandlerMonitor) IncBackupStorageWrites(outputType string) {
 	m.backupCounter.With(prometheus.Labels{"output_type": outputType}).Add(1)
 }
 
-func (m *HandlerMonitor) RegisterSegmentsChannelSizeGauge(nodeId string, clusterId string, egressId string, channelSizeFunction func() float64) {
+func (m *HandlerMonitor) RegisterSegmentsChannelSizeGauge(nodeId string, clusterId string, channelSizeFunction func() float64) {
 	segmentsUploadsGauge := prometheus.NewGaugeFunc(
 		prometheus.GaugeOpts{
 			Namespace:   "livekit",
 			Subsystem:   "egress",
 			Name:        "segments_uploads_channel_size",
 			Help:        "number of segment uploads pending in channel",
-			ConstLabels: prometheus.Labels{"node_id": nodeId, "cluster_id": clusterId, "egress_id": egressId},
+			ConstLabels: prometheus.Labels{"node_id": nodeId, "cluster_id": clusterId},
 		}, channelSizeFunction)
 	prometheus.MustRegister(segmentsUploadsGauge)
 }
 
-func (m *HandlerMonitor) RegisterPlaylistChannelSizeGauge(nodeId string, clusterId string, egressId string, channelSizeFunction func() float64) {
+func (m *HandlerMonitor) RegisterPlaylistChannelSizeGauge(nodeId string, clusterId string, channelSizeFunction func() float64) {
 	playlistUploadsGauge := prometheus.NewGaugeFunc(
 		prometheus.GaugeOpts{
 			Namespace:   "livekit",
 			Subsystem:   "egress",
 			Name:        "playlist_uploads_channel_size",
 			Help:        "number of playlist updates pending in channel",
-			ConstLabels: prometheus.Labels{"node_id": nodeId, "cluster_id": clusterId, "egress_id": egressId},
+			ConstLabels: prometheus.Labels{"node_id": nodeId, "cluster_id": clusterId},
 		}, channelSizeFunction)
 	prometheus.MustRegister(playlistUploadsGauge)
 }
