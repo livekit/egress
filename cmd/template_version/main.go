fe527bc50b4a9a29140d9cdb2b67907a2dad1d42
Benjamin Pracht <benjamin@livekit.io>
2025-09-10 11:40:21 -0700

    Tests

--- cmd/template_version/main.go
+++ cmd/template_version/main.go
@@ -1,4 +1,4 @@
-// Copyright 2023 LiveKit, Inc.
+// Copyright 2025 LiveKit, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,9 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package version
+package main
 
-const (
-	Version         = "1.10.0"
-	TemplateVersion = "1.10.0"
+import (
+	"fmt"
+
+	"github.com/livekit/egress/version"
 )
+
+func main() {
+	fmt.Println(version.TemplateVersion)
+}
