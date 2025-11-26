c3613784d4db9b5d0a1c60359de19873576ded0a
Milos Pesic <milos.pesic@livekit.io>
2025-11-26 17:30:32 +0100

    Adding logger for file outputs exceeding set of file size thresholds (#1079)

--- pkg/pipeline/sink/file.go
+++ pkg/pipeline/sink/file.go
@@ -15,14 +15,19 @@
 package sink
 
 import (
+	"context"
+	"os"
 	"path"
+	"time"
 
 	"github.com/livekit/egress/pkg/config"
+	"github.com/livekit/egress/pkg/errors"
 	"github.com/livekit/egress/pkg/gstreamer"
 	"github.com/livekit/egress/pkg/pipeline/builder"
 	"github.com/livekit/egress/pkg/pipeline/sink/uploader"
 	"github.com/livekit/egress/pkg/stats"
 	"github.com/livekit/egress/pkg/types"
+	"github.com/livekit/protocol/logger"
 )
 
 type FileSink struct {
@@ -31,6 +36,9 @@ type FileSink struct {
 	*uploader.Uploader
 
 	conf *config.PipelineConfig
+
+	cancelSizeLogger context.CancelFunc
+	sizeLoggerCtx    context.Context
 }
 
 func newFileSink(
@@ -52,17 +60,26 @@ func newFileSink(
 		return nil, err
 	}
 
+	ctx, cancel := context.WithCancel(context.Background())
+	p.AddOnStop(func() error {
+		cancel()
+		return nil
+	})
+
 	return &FileSink{
 		base: &base{
 			bin: fileBin,
 		},
-		FileConfig: o,
-		Uploader:   u,
-		conf:       conf,
+		FileConfig:       o,
+		Uploader:         u,
+		conf:             conf,
+		cancelSizeLogger: cancel,
+		sizeLoggerCtx:    ctx,
 	}, nil
 }
 
 func (s *FileSink) Start() error {
+	go s.monitorFileSize(s.sizeLoggerCtx)
 	return nil
 }
 
@@ -81,6 +98,10 @@ func (s *FileSink) UploadManifest(filepath string) (string, bool, error) {
 }
 
 func (s *FileSink) Close() error {
+	if s.cancelSizeLogger != nil {
+		s.cancelSizeLogger()
+	}
+
 	location, size, err := s.Upload(s.LocalFilepath, s.StorageFilepath, s.OutputType, false)
 	if err != nil {
 		return err
@@ -94,4 +115,49 @@ func (s *FileSink) Close() error {
 	}
 
 	return nil
+}
+
+func (s *FileSink) monitorFileSize(ctx context.Context) {
+	thresholds := []int64{
+		1 << 30,  // 1GB
+		3 << 30,  // 3GB
+		5 << 30,  // 5GB
+		10 << 30, // 10GB
+		20 << 30, // 20GB
+		50 << 30, // 50GB
+	}
+
+	ticker := time.NewTicker(time.Second * 30)
+	defer ticker.Stop()
+
+	nextThreshold := 0
+	statErrorLogged := false
+
+	for nextThreshold < len(thresholds) {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			info, err := os.Stat(s.LocalFilepath)
+			if err != nil {
+				if !statErrorLogged && !errors.Is(err, os.ErrNotExist) {
+					logger.Debugw("failed to stat filesink output", err, "filepath", s.LocalFilepath)
+					statErrorLogged = true
+				}
+				continue
+			}
+			statErrorLogged = false
+
+			pos := info.Size()
+			for nextThreshold < len(thresholds) && pos >= thresholds[nextThreshold] {
+				logger.Debugw(
+					"filesink size threshold exceeded",
+					"filepath", s.LocalFilepath,
+					"bytesWritten", pos,
+					"thresholdBytes", thresholds[nextThreshold],
+				)
+				nextThreshold++
+			}
+		}
+	}
 }
