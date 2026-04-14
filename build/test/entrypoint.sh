59158f84a1e44df17574092da066285b28b3bd40
David Colburn <xero73@gmail.com>
2026-04-14 13:18:41 -0400

    put entrypoint changes back in

--- build/test/entrypoint.sh
+++ build/test/entrypoint.sh
@@ -13,11 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -exo pipefail
+set -eo pipefail
 
 # Start pulseaudio
 rm -rf /var/run/pulse /var/lib/pulse /home/egress/.config/pulse /home/egress/.cache/xdgr/pulse
-pulseaudio -D --verbose --exit-idle-time=-1 --disallow-exit
+pulseaudio -D --verbose --exit-idle-time=-1 --disallow-exit > /dev/null 2>&1
 
 # Run RTSP server
 ./mediamtx > /dev/null 2>&1 &
