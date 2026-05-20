6dae68e33d6d2dab99844a8e8949437a1be56818
David Colburn <xero73@gmail.com>
2026-05-20 18:39:26 -0400

    remove duplicated download/delete code (#1231)

--- test/download.go
+++ test/download.go
@@ -17,28 +17,15 @@
 package test
 
 import (
-	"context"
 	"encoding/json"
-	"fmt"
-	"io"
-	"net/url"
 	"os"
 	"testing"
 
-	"cloud.google.com/go/storage"
-	"github.com/Azure/azure-storage-blob-go/azblob"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	awsConfig "github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
-	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/googleapis/gax-go/v2"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/api/option"
 
 	"github.com/livekit/egress/pkg/config"
 	"github.com/livekit/protocol/logger"
-	lkstorage "github.com/livekit/storage"
+	"github.com/livekit/storage"
 )
 
 func loadManifest(t *testing.T, c *config.StorageConfig, localFilepath, storageFilepath string) *config.Manifest {
@@ -56,133 +43,36 @@ func loadManifest(t *testing.T, c *config.StorageConfig, localFilepath, storageF
 }
 
 func download(t *testing.T, c *config.StorageConfig, localFilepath, storageFilepath string, delete bool) {
-	if c != nil {
-		if c.S3 != nil {
-			logger.Debugw("s3 download", "localFilepath", localFilepath, "storageFilepath", storageFilepath)
-			downloadS3(t, c.S3, localFilepath, storageFilepath, delete)
-		} else if c.GCP != nil {
-			logger.Debugw("gcp download", "localFilepath", localFilepath, "storageFilepath", storageFilepath)
-			downloadGCP(t, c.GCP, localFilepath, storageFilepath, delete)
-		} else if c.Azure != nil {
-			logger.Debugw("azure download", "localFilepath", localFilepath, "storageFilepath", storageFilepath)
-			downloadAzure(t, c.Azure, localFilepath, storageFilepath, delete)
-		}
+	if c == nil {
+		return
 	}
-}
 
-func downloadS3(t *testing.T, conf *lkstorage.S3Config, localFilepath, storageFilepath string, delete bool) {
-	file, err := os.Create(localFilepath)
-	require.NoError(t, err)
-	defer file.Close()
-
-	awsConf, err := awsConfig.LoadDefaultConfig(context.Background(), func(o *awsConfig.LoadOptions) error {
-		o.Region = conf.Region
-		o.Credentials = credentials.StaticCredentialsProvider{
-			Value: aws.Credentials{
-				AccessKeyID:     conf.AccessKey,
-				SecretAccessKey: conf.Secret,
-				SessionToken:    conf.SessionToken,
-			},
-		}
-
-		return nil
-	})
-	require.NoError(t, err)
-	s3Client := s3.NewFromConfig(awsConf)
-
-	_, err = manager.NewDownloader(s3Client).Download(
-		context.Background(),
-		file,
-		&s3.GetObjectInput{
-			Bucket: aws.String(conf.Bucket),
-			Key:    aws.String(storageFilepath),
-		},
+	var (
+		conf     storage.Config
+		provider string
 	)
+	switch {
+	case c.S3 != nil:
+		conf, provider = c.S3, "s3"
+	case c.GCP != nil:
+		conf, provider = c.GCP, "gcp"
+	case c.Azure != nil:
+		conf, provider = c.Azure, "azure"
+	case c.AliOSS != nil:
+		conf, provider = c.AliOSS, "alioss"
+	default:
+		return
+	}
+
+	logger.Debugw(provider+" download", "localFilepath", localFilepath, "storageFilepath", storageFilepath)
+
+	s, err := storage.New(conf)
+	require.NoError(t, err)
+
+	_, err = s.DownloadFile(localFilepath, storageFilepath)
 	require.NoError(t, err)
 
 	if delete {
-		_, err = s3Client.DeleteObject(context.Background(), &s3.DeleteObjectInput{
-			Bucket: aws.String(conf.Bucket),
-			Key:    aws.String(storageFilepath),
-		})
-		require.NoError(t, err)
-	}
-}
-
-func downloadAzure(t *testing.T, conf *lkstorage.AzureConfig, localFilepath, storageFilepath string, delete bool) {
-	credential, err := azblob.NewSharedKeyCredential(
-		conf.AccountName,
-		conf.AccountKey,
-	)
-	require.NoError(t, err)
-
-	pipeline := azblob.NewPipeline(credential, azblob.PipelineOptions{
-		Retry: azblob.RetryOptions{
-			Policy:        azblob.RetryPolicyExponential,
-			MaxTries:      maxRetries,
-			MaxRetryDelay: maxDelay,
-		},
-	})
-	sUrl := fmt.Sprintf("https://%s.blob.core.windows.net/%s", conf.AccountName, conf.ContainerName)
-	azUrl, err := url.Parse(sUrl)
-	require.NoError(t, err)
-
-	containerURL := azblob.NewContainerURL(*azUrl, pipeline)
-	blobURL := containerURL.NewBlobURL(storageFilepath)
-
-	file, err := os.Create(localFilepath)
-	require.NoError(t, err)
-	defer file.Close()
-
-	err = azblob.DownloadBlobToFile(context.Background(), blobURL, 0, 0, file, azblob.DownloadFromBlobOptions{
-		BlockSize:   4 * 1024 * 1024,
-		Parallelism: 16,
-		RetryReaderOptionsPerBlock: azblob.RetryReaderOptions{
-			MaxRetryRequests: 3,
-		},
-	})
-	require.NoError(t, err)
-
-	if delete {
-		_, err = blobURL.Delete(context.Background(), azblob.DeleteSnapshotsOptionNone, azblob.BlobAccessConditions{})
-		require.NoError(t, err)
-	}
-}
-
-func downloadGCP(t *testing.T, conf *lkstorage.GCPConfig, localFilepath, storageFilepath string, delete bool) {
-	ctx := context.Background()
-	var client *storage.Client
-
-	var err error
-	if conf.CredentialsJSON != "" {
-		client, err = storage.NewClient(ctx, option.WithCredentialsJSON([]byte(conf.CredentialsJSON)))
-	} else {
-		client, err = storage.NewClient(ctx)
-	}
-	require.NoError(t, err)
-	defer client.Close()
-
-	file, err := os.Create(localFilepath)
-	require.NoError(t, err)
-	defer file.Close()
-
-	rc, err := client.Bucket(conf.Bucket).Object(storageFilepath).Retryer(
-		storage.WithBackoff(
-			gax.Backoff{
-				Initial:    minDelay,
-				Max:        maxDelay,
-				Multiplier: 2,
-			}),
-		storage.WithPolicy(storage.RetryAlways),
-	).NewReader(ctx)
-	require.NoError(t, err)
-
-	_, err = io.Copy(file, rc)
-	_ = rc.Close()
-	require.NoError(t, err)
-
-	if delete {
-		err = client.Bucket(conf.Bucket).Object(storageFilepath).Delete(context.Background())
-		require.NoError(t, err)
+		require.NoError(t, s.DeleteObject(storageFilepath))
 	}
 }
